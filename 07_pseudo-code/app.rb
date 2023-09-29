1f857912cda828b4576b4ed99c536ac89adf5b09
Nicholas Schaefer <nick@nicholasschaefer.com>
2023-09-29 13:41:41 +0000

    completed exercise lesson_2-7-pseudo-code

---
+++ 07_pseudo-code/app.rb
@@ -0,0 +1,75 @@
+#Pseudo Code for the following Problems
+=begin
+
+###a method that returns the sum of two integers### 
+
+
+START
+
+GET 2 integers from user, validate that they are integers
+SET those two integers in an array called numbers
+SET variable sum equal to nil
+SET array_index = 0
+
+WHILE iterator <=1
+  SET sum = value of array at index value of array_index
+  go to the next iteration
+
+PRINT sum
+
+END
+
+
+
+###a method that takes an array of strings, and returns a string that is all those strings concatenated together###
+
+START
+SET array_of_strings = to an array with several strings
+SET count_aostrings = to the number of items in the array_of_strings
+SET concatenated_string = to an empty string
+SET current_string = to an empty string
+SET iterations = to 0
+
+WHILE iterations < count_aostrings
+  SET current_string = to the value of array_of_strings at index iterations
+  SET concatenated_string = to value of concatenated_string + value of current_string
+
+PRINT concatenated_string
+
+END
+
+
+
+
+=end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+=end
+
