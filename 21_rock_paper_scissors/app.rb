a780c2f390a248768088dbff0c8e90b2e7346bec
Nicholas Schaefer <nick@nicholasschaefer.com>
2023-10-03 13:32:24 +0000

    completed exercise lesson_2-21-rock-paper-scissors

---
+++ 21_rock_paper_scissors/app.rb
@@ -0,0 +1,94 @@
+VALID_CHOICES = ["rock", "paper", "scissors"]
+
+def display_prompt(text_to_prompt)
+  puts "=> #{text_to_prompt}"
+end
+
+def randomly_generated_choice
+  VALID_CHOICES.sample
+end
+
+def request_user_choice
+  display_prompt("Your move! Enter either 'rock', 'paper', or 'scissors'")
+end
+
+def display_winner(winner:, user_throw:, computer_throw:)
+  display_prompt("User choice: #{user_throw.capitalize}")
+  display_prompt("Computer choice: #{computer_throw.capitalize}")
+  display_prompt("#{winner.capitalize} is the winner!")
+end
+
+def display_tie_warning
+  display_prompt("Because you got a tie! You'll have to play again")
+end
+
+def request_play_again
+  user_wants_to_play_again = nil
+  loop do
+    display_prompt("Would you like to play again?(Y / N)")
+    user_play_again_choice = gets.chomp.downcase
+    user_wants_to_play_again =  case user_play_again_choice
+                                when "y" then true
+                                when "n" then false
+                                end
+    break unless user_wants_to_play_again.nil?
+    display_prompt("I didn't understand your input, please select again.")
+  end
+  user_wants_to_play_again
+end
+
+def update_user_choice
+  user_selection = ""
+
+  loop do
+    request_user_choice()
+    user_selection = gets.chomp.downcase
+    break if VALID_CHOICES.include?(user_selection)
+    display_prompt("Idiot! You didn't select a valid option. Try again...")
+  end
+
+  user_selection
+end
+
+def update_computer_choice
+  randomly_generated_choice()
+end
+
+def update_round_winner(user_selection, computer_selection)
+  case user_selection
+  when  "rock"
+    return "user" if computer_selection == "scissors"
+    return "computer" if computer_selection == "paper"
+    return "nobody" if computer_selection == "rock"
+  when  "paper"
+    return "user" if computer_selection == "rock"
+    return "computer" if computer_selection == "scissors"
+    return "nobody" if computer_selection == "paper"
+  when  "scissors"
+    return "user" if computer_selection == "paper"
+    return "computer" if computer_selection == "rock"
+    return "nobody" if computer_selection == "scissors"
+  end
+end
+
+def my_app
+  loop do
+    round_winner = ""
+    loop do
+      user_choice = update_user_choice()
+      computer_choice = update_computer_choice()
+      round_winner = update_round_winner(user_choice, computer_choice)
+      display_winner( \
+        user_throw: user_choice, \
+        computer_throw: computer_choice, \
+        winner: round_winner \
+      )
+      break unless round_winner == "nobody"
+      display_tie_warning()
+    end
+    break if request_play_again() == false
+  end
+  display_prompt("Ok, goodbye!")
+end
+
+my_app()
